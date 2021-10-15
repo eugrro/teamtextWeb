1e1964851d32d9f37d8de633614e0bb373e549e9
eugroo <eugene.rozental@gmail.com>
2021-10-15 12:14:20 -0400

    more server connection

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var app = express();
 const keys = require("./keys.js")
+var ObjectId = require('mongodb').ObjectId;
 /////////////////////////////////////////////////
 
 const MongoClient = require('mongodb').MongoClient;
@@ -59,7 +60,9 @@ app.get("/join/:teamID", async function (req, res) {
     res.render('joinTeam.html', { teamID: req.params.teamID });
 });
 app.post("/api/createUser", async function (req, res) {
-
+    //code to check if the user exists (by phone number or email)
+    console.log("=========== Create User ===========");
+    console.log(req.body);
     db.collection("users").insertOne(req.body, function (err, result) {
         if (err) throw err;
         console.log(result);
@@ -68,7 +71,8 @@ app.post("/api/createUser", async function (req, res) {
     //Code needed tp verify that user allows texts 
 });
 app.post("/api/removeUser", async function (req, res) {
-
+    console.log("=========== Remove User ===========");
+    console.log(req.body);
     db.collection("users").deleteOne({ "uid": req.body.uid }, function (err, result) {
         if (err) throw err;
         console.log(result);
@@ -76,7 +80,8 @@ app.post("/api/removeUser", async function (req, res) {
     });
 });
 app.post("/api/createTeam", async function (req, res) {
-
+    console.log("=========== Create Team ===========");
+    console.log(req.body);
     db.collection("teams").insertOne({ "leader": req.body.leader, "members": [] }, function (err, result) {
         if (err) throw err;
         console.log(result);
@@ -84,18 +89,45 @@ app.post("/api/createTeam", async function (req, res) {
     });
 });
 app.post("/api/joinTeam", async function (req, res) {
-
-    db.collection("teams").insertOne({ "tid": req.body.tid }, { "$push": { "members": req.body.mid } }, function (err, result) {
+    console.log("=========== Join Team ===========");
+    console.log(req.body);
+    db.collection("teams").updateOne({ "tid": req.body.tid }, { "$push": { "members": req.body.mid } }, function (err, result) {
         if (err) throw err;
         console.log(result);
         res.send(result);
     });
 });
 app.post("/api/deleteTeam", async function (req, res) {
-
-    db.collection("team").deleteOne({ "tid": req.body.tid }, function (err, result) {
+    console.log("=========== Delete Team ===========");
+    console.log(req.body);
+    db.collection("teams").deleteOne({ "tid": req.body.tid }, function (err, result) {
         if (err) throw err;
         console.log(result);
         res.send(result);
+    });
+});
+app.post("/api/getTeamMembers", async function (req, res) {
+    //should accept team leader id and make sure they are allowed to make this call
+    console.log("=========== Get Team Members ===========");
+    console.log(req.body);
+    db.collection("teams").findOne({ "tid": req.body.tid }, async function (err, result) {
+        if (err) throw err;
+        console.log(result);
+        membersIDList = result["members"];
+        modifiedIDArr = membersIDList.map(id => ObjectId(id));
+        db.collection("users").find({ '_id': { "$in": modifiedIDArr } }).toArray(function (err, result2) {
+            if (err) throw err;
+            //only return the id and name
+            result2.forEach(function (element, index) {
+                result2[index] = { "mid": element["_id"], "fName": element["fName"], "lName": element["lName"] };
+            });
+            //should maybe sort based on when the last message was sent
+            console.log(result2);
+            res.send(result2);
+        });
+
+
+        //console.log(await result2.toArray());
+        //res.send(await result2.toArray());
     });
 });
