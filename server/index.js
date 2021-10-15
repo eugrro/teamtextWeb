920daed6221cb633500d72dce057be480c2e1384
eugroo <eugene.rozental@gmail.com>
2021-10-15 10:41:57 -0400

    fixed key leak

--- server/index.js
+++ server/index.js
@@ -1,8 +1,18 @@
 var express = require("express");
 var app = express();
+const keys = require("./keys.js")
+/////////////////////////////////////////////////
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = keys["mongoUri"];
+
+let db;
+/////////////////////////////////////////////////
+var bodyParser = require('body-parser');
 app.use(express.json({ extended: false }));
 var fs = require('fs');
-const keys = require("./keys.js")
+const path = require("path");
+
 // Load the AWS SDK for Node.js
 //var AWS = require('aws-sdk');
 
@@ -17,18 +27,17 @@ const keys = require("./keys.js")
 
 //s3 = new AWS.S3();
 
-//
-/*require("./db/db").connectDB(function (err) {
-    if (err) {
-        console.log("Connection to MongoDB was unsuccessful");
-        process.exit(1);
-    } else {
-        console.log("Connected to MongoDB database");
+MongoClient.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true }, (err, _db) => {
+    if (!err) {
+        console.log("Successfully connected to MongoDB");
+        db = _db.db("db");
     }
-});*/
-var testingLocally = false;
+    else
+        console.log("Error in the connectivity");
+})
+var testingLocally = true;
 
-if (testingLocally == true) {
+if (testingLocally) {
     var port = 3000;
     var ip = "192.168.1.150";
 
@@ -42,3 +51,52 @@ if (testingLocally == true) {
 } else {
     var app = require("./server.js");
 }
+
+app.use(express.static(__dirname + './../web/'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+app.set('views', __dirname + './../web/');
+app.get("/join/:teamID", async function (req, res) {
+    res.render('joinTeam.html', { teamID: req.params.teamID });
+});
+app.post("/api/createUser", async function (req, res) {
+
+    db.collection("users").insertOne(req.body, function (err, result) {
+        if (err) throw err;
+        console.log(result);
+        res.send(result);
+    });
+    //Code needed tp verify that user allows texts 
+});
+app.post("/api/removeUser", async function (req, res) {
+
+    db.collection("users").deleteOne({ "uid": req.body.uid }, function (err, result) {
+        if (err) throw err;
+        console.log(result);
+        res.send(result);
+    });
+});
+app.post("/api/createTeam", async function (req, res) {
+
+    db.collection("teams").insertOne({ "leader": req.body.leader, "members": [] }, function (err, result) {
+        if (err) throw err;
+        console.log(result);
+        res.send(result);
+    });
+});
+app.post("/api/joinTeam", async function (req, res) {
+
+    db.collection("teams").insertOne({ "tid": req.body.tid }, { "$push": { "members": req.body.mid } }, function (err, result) {
+        if (err) throw err;
+        console.log(result);
+        res.send(result);
+    });
+});
+app.post("/api/deleteTeam", async function (req, res) {
+
+    db.collection("team").deleteOne({ "tid": req.body.tid }, function (err, result) {
+        if (err) throw err;
+        console.log(result);
+        res.send(result);
+    });
+});
